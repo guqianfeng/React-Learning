36fcad4145ff2c8498df06aec4cb2ddc1196dd9d
qianfengg <fred_gqf0221@163.com>
2020-01-07 20:35:04 +0800

    补全路由代码

--- app/src/router/route_list.js
+++ app/src/router/route_list.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 import Index from '../view/index/index'
+import Course from '../view/course/index'
+import Lectruer from '../view/lectruer/index'
+import Login from '../view/login/index'
+import Work from '../view/work/index'
 
 const routeList = [
     {
@@ -9,6 +13,38 @@ const routeList = [
         exact: true,
         render(props){
             return <Index {...props}/>
+        }
+    },
+    {
+        name: "课程",
+        path: "/course",
+        exact: true,
+        render(props){
+            return <Course {...props}/>
+        }
+    },
+    {
+        name: "讲师团队",
+        path: "/lectruer",
+        exact: true,
+        render(props){
+            return <Lectruer {...props}/>
+        }
+    },
+    {
+        name: "登录注册",
+        path: "/login",
+        exact: true,
+        render(props){
+            return <Login {...props}/>
+        }
+    },
+    {
+        name: "作品",
+        path: "/work",
+        exact: true,
+        render(props){
+            return <Work {...props}/>
         }
     },
 ];
--- app/src/view/work/index.js
+++ app/src/view/work/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export default () => {
+export default (props) => {
+    console.log(props)
     return (
         <div>
             <h1>作品</h1>
