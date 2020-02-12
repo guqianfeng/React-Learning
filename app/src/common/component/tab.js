a47fd6cc8e2db97766cf31103ba1a8cb58a0ab0e
qianfengg <fred_gqf0221@163.com>
2020-02-12 16:16:19 +0800

    通用组件2的学习

--- app/src/common/component/tab.js
+++ app/src/common/component/tab.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import BScroll from 'better-scroll';
 
 export default function Tab(props){
     // console.log(props);
     let {data, render} = props;
+    let bScroll = null;
+    let bannerWrap = useRef(null);
+    const [now, setNow] = useState(0);
+    useEffect(() => {
+        let timer = 0;
+        bScroll = new BScroll(bannerWrap.current, {
+            scrollX: true,
+            scrollY: false,
+            eventPassthrough: 'vertical',
+            momentum: false,
+            snap: {
+                loop: true,
+            }
+        });
+        bScroll.on('scrollEnd', () => {
+            // console.log(bScroll.getCurrentPage());
+            setNow(bScroll.getCurrentPage().pageX);
+        })
+        timer = setInterval(() => {
+            bScroll.next(200);
+        }, 2000)
+        bannerWrap.current.addEventListener('touchstart', () => {
+            clearInterval(timer);
+        })
+        bannerWrap.current.addEventListener('touchend', () => {
+            timer = setInterval(() => {
+                bScroll.next(200);
+            }, 2000)
+        })
+        return () => {
+            //组件摧毁时也需要关闭定时器
+            clearInterval(timer);
+        }
+    }, [])
     return (
         <div className="banner">
-            <div className="banner_img">
+            <div className="banner_img" ref={bannerWrap}>
                 <ul className="banner_list clearfix">
                     {data.map((item, index) => <li key={index}>{render(item)}</li>)}
                 </ul>
             </div> 
             <ul className="banner_nav">
-                {data.map((item, index) => <li key={index}></li>)}
+                {data.map((item, index) => {
+                    return (
+                        <li 
+                            key={index} 
+                            className={index===now ? 'active' : ''}>    
+                        </li>
+                    )
+                })}
             </ul>
         </div>
     )
