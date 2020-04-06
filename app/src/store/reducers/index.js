ea656007e726d9cd08b9110a3e76eee353a868fb
qianfengg <fred_gqf0221@163.com>
2020-04-06 16:55:29 +0800

    讲师幻灯片数据获取

--- app/src/store/reducers/index.js
+++ app/src/store/reducers/index.js
@@ -1,7 +1,9 @@
 import getUser from './login'
 import works from './works'
+import lecturers from './lecturers'
 
 export default {
     getUser,
     works,
+    lecturers
 }
--- app/src/view/lecturer/index.js
+++ app/src/view/lecturer/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default () => {
-    return (
-        <div>
-            <h1>讲师团队</h1>
-        </div>
-    )
+import { connect } from 'react-redux'
+import Frame from '../../common/component/frame'
+import Tab from '../../common/component/tab'
+import getLecturers from '../../store/action/getLecturers'
+
+import '../../common/css/teacher.css'
+
+function Lecturer (props) {
+  let { data, dispatch } = props;
+  let newData = []; //一共拿到9个数据，3个一组
+  useEffect(() => {
+    dispatch(getLecturers())
+  }, [])
+  // console.log(data)
+  for (let i = 0; i < data.length; i += 3) {
+    newData.push([
+      data[i],
+      data[i + 1],
+      data[i + 2]
+    ])
+  }
+  console.log(newData)
+  return (
+    <Frame>
+      <div className="teacher_banner">
+        <h2><span>妙味团队</span></h2>
+        { data.length < 1 ? '' : '' }
+      </div>
+    </Frame>
+  )
 }
+
+export default connect(state => state.lecturers)(Lecturer)
