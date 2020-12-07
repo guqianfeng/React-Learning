8ae37cf2ab8b7669bb2a753cda3961271a3bf522
guqianfeng <847523167@qq.com>
2020-12-07 15:49:10 +0800

    Add files via upload

--- 全家桶/redux相关/gqf-redux.js
+++ 全家桶/redux相关/gqf-redux.js
@@ -1,26 +1,59 @@
-export function createStore (reducer) {
+export function createStore(reducer, enhance) {
+  if (enhance) {
+    return enhance(createStore)(reducer);
+  }
   let currentState;
   let listeners = [];
-
   function getState () {
     return currentState;
   }
 
   function dispatch (action) {
-    currentState = reducer(currentState, action)
-    listeners.forEach(fn => fn())
+    currentState = reducer(currentState, action);
+    listeners.forEach(cb => cb());
     return action;
   }
 
-  function subscribe(cb) {
+  function subscribe (cb) {
     listeners.push(cb)
   }
 
-  dispatch({type: 'gqf-redux~~'})
-
+  dispatch({type: 'gqf nb'})
+  
   return {
     getState,
     dispatch,
-    subscribe,
+    subscribe
+  }
+}
+
+function compose (...fns) {
+  console.log(fns, fns.length)
+  if (!fns.length) {
+    return arg => arg
+  }
+  if (fns.length === 1) {
+    return fns[0];
+  }
+  return fns.reduce((left, right) => (...args) => right(left(...args)));
+}
+
+export function applyMiddleware (...middlewares) {
+  return createStore => (...args) => {
+    const store = createStore(...args);
+    let dispatch = store.dispatch;
+    let midAPI = {
+      getState: store.getState,
+      dispatch: (...args) => dispatch(...args) 
+    }
+    console.log(midAPI, middlewares)
+    let chain = middlewares.map(mw => mw(midAPI));
+    console.log(compose)
+    dispatch = compose(...chain)(dispatch);
+
+    return {
+      ...store,
+      dispatch
+    }
   }
 }
