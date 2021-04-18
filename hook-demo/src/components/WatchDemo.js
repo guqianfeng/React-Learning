0befb71ab115b2e73602cf90dd4acaae675d5b9c
qianfengg <fred_gqf0221@163.com>
2021-04-18 16:57:42 +0800

    update demo

--- hook-demo/src/components/WatchDemo.js
+++ hook-demo/src/components/WatchDemo.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react'
 import useWatch from '../hooks/use-watch'
 
 export default function WatchDemo() {
+    const [prevCount, setPrevCount] = useState()
     const [count, setCount] = useState(0)
     const stop = useWatch(count, (prev) => {
         console.log('发生变化了，当前值是 ', count, '上一次值是 ', prev)
+        setPrevCount(prev)
     }, {
         immediate: !true
     })
@@ -12,7 +14,10 @@ export default function WatchDemo() {
         <div>
             <h1>Watch Demo</h1>
             <div>
-                {count}
+                上一个值 - {prevCount !== undefined ? prevCount : '上个值还没有呢'}
+            </div>
+            <div>
+                当前值 - {count}
             </div>
             <div>
                 <button onClick={() => {
