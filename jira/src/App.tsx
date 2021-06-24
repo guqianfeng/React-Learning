f0731ab45f35dc6957c70223720c5dbe17710c87
qianfengg <fred_gqf0221@163.com>
2021-06-24 16:01:43 +0800

    test

--- jira/src/App.tsx
+++ jira/src/App.tsx
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
